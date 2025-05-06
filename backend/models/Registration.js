59fe1e0057cda945946517f66ae162524d450f2a
Keerthi Nerella <22wh1a6629@bvrithyderabad.edu.in>
2025-05-06 16:23:35 +0530

    mess

---
+++ backend/models/Registration.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// Define the Registration schema
+const registrationSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  admin: String,
+  mobile: String,
+  lockerBoxes: Number,
+  lockersPerBox: Number
+});
+
+// Create the Registration model
+module.exports = mongoose.model('Registration', registrationSchema);
